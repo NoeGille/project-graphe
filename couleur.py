2743bdefcc38dde21f9abcc97ca381306b7b16b1
NoeGille <115892813+NoeGille@users.noreply.github.com>
2023-01-26 17:28:26 +0100

    Delete couleur.py

--- couleur.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-class Couleur(Enum):
-    '''Enumération des couleurs qui représente l'état d'exploration d'un sommet pour
-    le parcours en profondeur'''
-    BLANC = 0
-    GRIS = 1
-    NOIR = 2
